d1c70a9fa318f362c24aaf97e6963e56bc89541f
Phong Do <dominhphong306@gmail.com>
2026-01-07 07:57:48 +0700

    feat: add code example for case 1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('@CASE_1', async ({ page }) => {
+    console.log("Code from main branch - case 1");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://material.playwrightvn.com/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Bài học 4: Personal notes' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Personal Notes' })).toBeVisible();
+test('@CASE_2', async ({ page }) => {
+    console.log("Code from main branch - case 2");
 });
